2d9cedac33c7dfa6b17c5dd168064fa7afad315f
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-04 18:44:04 +0000

    added prototypes

--- main.h
+++ main.h
@@ -1,23 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 extern char **environ;
 
-
 char **line_devider(char *buffer, char **arr);
+char *get_input(void);
 void execute_command(char *buffer, char **argv);
 void handle_path(char *buffer, char **argv);
-char *get_input(void);
-int path_handler(char *buffer);
 void handle_env(char *buffer);
-
+int path_handler(char *buffer);
 #endif
