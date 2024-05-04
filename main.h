b79eb796ebb6a97ac9f1def79243b0fd3cc4fc88
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-04 17:33:37 +0000

    added new prototypes and environ

--- main.h
+++ main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
@@ -10,8 +8,14 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+extern char **environ;
 
 
+char **line_devider(char *buffer, char **arr);
+void execute_command(char *buffer, char **argv);
+void handle_path(char *buffer, char **argv);
+char *get_input(void);
 int path_handler(char *buffer);
+void handle_env(char *buffer);
 
 #endif
