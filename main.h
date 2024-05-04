8cacfa194477d368c27189541a64f55395b875fa
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-04 14:38:03 +0000

    created header file

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+
+int path_handler(char *buffer);
+
+#endif
